933f66867f234e4bc472b55fd49fec18ab9fb9d4
Millan Martinez <milmararco@gmail.com>
2021-06-05 18:20:20 +0200

    de momento dejo así los js

--- js/check-page.js
+++ js/check-page.js
@@ -1,19 +1,20 @@
+/**
+ * Compureba que haya un usuario registrado
+ * Si hay un usuario registrado que está en index.html le redirecciona a show_vols.html
+ * Si no hay nadie registrado que esté en cualquier sitio que no sea index.html le redirecciona a index.html
+ */
 firebase.auth().onAuthStateChanged((user) => {
     if (user) {
         // User is signed in, see docs for a list of available properties
         // https://firebase.google.com/docs/reference/js/firebase.User
         var uid = user.uid;
         // The user has to click the logout button to reach the login
-        console.log("signed in")
-        console.log(document.location.pathname)
         if(document.location.pathname == "/"){
             document.location.href = "html/show_vols.html"
         }
     } else {
         // User is signed out
-        // If the user is not signed he cant reach anything that's not index.html        
-        console.log("signed out")
-        console.log(document.location.pathname)
+        // If the user is not signed he cant reach anything that's not index.html
         if(document.location.pathname != "/"){
             document.location.href = "/"
         }
--- js/loading.js
+++ js/loading.js
@@ -1,6 +1,12 @@
 let spinnerWrapper = document.querySelector(".spinner-wrapper")
 
+/**
+ * Esto se ejecuta al cargar la página
+ */
 window.addEventListener('load', () => {
-    // A bit of delay
+    /**
+     * Le pongo un delay de medio segundo porque si le pongo que dure hasta que todo se cargue
+     * se podrá ver las imágenes de las tarjetas porque se carga de forma asíncrona
+     * */
     setTimeout(() => {spinnerWrapper.style.display = 'none'}, 500);
 })
